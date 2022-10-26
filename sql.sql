f732a8fc9c2f352745c24e03904071874851095b
BurntJannings <82676805+BurntJannings@users.noreply.github.com>
2022-10-25 20:09:14 -0600

    Update sql.sql

--- sql.sql
+++ sql.sql
@@ -14,3 +14,11 @@ CREATE TABLE IF NOT EXISTS `communityservice` (
   `communityservice` varchar(100) NOT NULL DEFAULT '0',
   `servicecount` varchar(100) NOT NULL DEFAULT '0'
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1 ROW_FORMAT=DYNAMIC;
+
+CREATE TABLE IF NOT EXISTS `society_ledger` (
+  `job` longtext DEFAULT NULL,
+  `ledger` int(11) NOT NULL DEFAULT 0
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+INSERT INTO `society_ledger` (`job`, `ledger`) VALUES
+	('police', 0);
