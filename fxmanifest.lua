1a92cbba34ebb9717e1e9eb7fd9a4f02f88a2a7b
BurntJannings <82676805+BurntJannings@users.noreply.github.com>
2023-01-08 01:38:45 -0700

    Update fxmanifest.lua

--- fxmanifest.lua
+++ fxmanifest.lua
@@ -1,13 +1,17 @@
-fx_version "adamant"
-games {"rdr3"}
+game 'rdr3'
+fx_version 'adamant'
 rdr3_warning 'I acknowledge that this is a prerelease build of RedM, and I am aware my resources *will* become incompatible once RedM ships.'
 
-server_scripts {
-	'config.lua',
-	'server/server.lua'
+client_script {
+    'client/client.lua'
 }
 
-client_scripts {
-	'config.lua',
-	'client/client.lua'
+server_script {
+    'server/server.lua'
+}
+
+shared_script {
+    'config.lua',
+    'locale.lua',
+    'en.lua'
 }
